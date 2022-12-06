ad29542a5f5a53a85c0badfc59e4f2ad9e7648bb
Horattiu <horatiu.oltn@gmail.com>
2022-12-06 21:12:54 +0200

    final.

--- src/Components/CoverImg.js
+++ src/Components/CoverImg.js
@@ -5,8 +5,7 @@ const CoverImg = () => {
   return (
     <div className="cover-container">
       <img src={require("./img/cover.jpg")} className="cover-img" />
-      <div className="text-container"
-      >
+      <div className="text-container" >
         <h2 data-aos="zoom-in" className="cover-text"> hi,</h2>
         <h2 className="cover-text"> I'm Oltean Horatiu</h2>
         <h2 className="cover-text">aspiring web developer</h2>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -48,7 +48,7 @@ function Projects() {
             </p>
             <a
               className="blank"
-              href="https://elaborate-cascaron-24c525.netlify.app"
+              href="https://banking-app-final.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -90,7 +90,7 @@ function Projects() {
             </p>
             <a
               className="blank"
-              href="https://elaborate-cascaron-24c525.netlify.app"
+              href="https://omnifood-web-final.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -100,24 +100,7 @@ function Projects() {
         </div>
 
         <div className="project-box">
-          <img
-            className="ss-project"
-            src={require("./img/omnifood-thumbnail.jpg")}
-          ></img>
-          <div className="overlay-content">
-            <p className="content-text">
-              first freelance project i did for a client / furniture company
-              using react
-            </p>
-            <a
-              className="blank"
-              href="https://elaborate-cascaron-24c525.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="btn-2">View live demo</button>
-            </a>
-          </div>
+
         </div>
 
         <div className="project-box"></div>
